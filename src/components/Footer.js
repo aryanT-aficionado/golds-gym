33016283dbe35760702365e59495956c78745ab4
aryanT-aficionado <aryanlucifer88@gmail.com>
2022-06-23 14:05:21 +0530

    Intial Final Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="Logo" width="200px" height="40px" />
+        <Typography color="#000" variant="h5" pb="40px">
+          Made by{" "}
+          <a
+            style={{
+              textDecoration: "none",
+              color: "#0d236c",
+            }}
+            color="#000"
+            href={`https://www.linkedin.com/in/aryan-tiwari-5276351a6/`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Aryan Tiwari
+          </a>{" "}
+          💪🏻🚴🏻‍♀️
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
